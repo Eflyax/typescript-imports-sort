b9e6ed85f0ce8271a5c014bf8a209ad9d16f42c4
Jakub Záruba <jakuba.zaruba@orgis.cz>
2024-02-15 09:26:40 +0100

    WIP

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,60 +1,81 @@
 import {ParsedGroup} from './types';
 import type {IParsedNode} from './types';
 
-const
-	name = `((?!\\d)(?:(?!\\s)[$\\w\\u0080-\\uFFFF]|\\\\u[\\da-fA-F]{4}|\\\\u\\{[\\da-fA-F]+\\})+)`,
-	ws = `\\s`,
-	spaceNoReturns = `[^\\S\\r\\n]`,
-	namespaceToken = `\\*\\s+as\\s+(${name})`,
-	defaultImportToken = name,
-	destructingImportToken = `(${name})(\\s+as\\s+(${name}))?`,
-	destructingImport = `{(${ws}*${destructingImportToken}(${ws}*,${ws}*${destructingImportToken})*${ws}*,?${ws}*)}`,
-	defaultAndDestructingImport = `${defaultImportToken}${ws}*,${ws}*${destructingImport}`,
-	combinedImportTypes = `(${namespaceToken}|${defaultImportToken}|${destructingImport}|${defaultAndDestructingImport})`,
-	inlineComment = `(${spaceNoReturns}*[\\/]{2}.*)?`,
-	importRegexString = `^import(\\s+type)?\\s+(${combinedImportTypes}\\s+from\\s+)?['"]([~@\\w\\\\/\.-]+)['"];?${inlineComment}\\r?\\n?`,
-	importRegex = new RegExp(importRegexString, 'gm'),
-	destructingImportTokenRegex = new RegExp(destructingImportToken);
+export class Parser {
 
-const parseDestructiveImports = (destructiveImports) => {
-	if (!destructiveImports) {
-		return null;
+	private destructingImportTokenRegex: RegExp;
+	private importRegex: RegExp;
+
+	constructor() {
+		this.initRegex();
 	}
 
-	return (destructiveImports.toString())
-		.split(',')
-		.map((destructiveImport) => {
-			const match = destructingImportTokenRegex.exec(destructiveImport);
-			return !match ? null : { alias: match[4], importName: match[1] };
-		})
-		.filter((destructiveImport) => !!destructiveImport?.importName);
-};
+	parseDestructiveImports = (destructiveImports) => {
+		if (!destructiveImports) {
+			return null;
+		}
 
-export const parseImportNodes = (source: string): Array<IParsedNode> => {
-	importRegex.lastIndex = 0;
+		return (destructiveImports.toString())
+			.split(',')
+			.map((destructiveImport) => {
+				const match =
+					this.destructingImportTokenRegex.exec(destructiveImport);
 
-	const
-		imports = [];
+				return !match
+					? null
+					: {
+						alias: match[ParsedGroup.NamespaceImport],
+						importName: match[ParsedGroup.TypeKeyword]
+					};
+			})
+			.filter((destructiveImport) => !!destructiveImport?.importName);
+	};
 
-	if (/(disable-sort-imports)/g.test(source)) {
-		return [];
+	parseImportNodes = (source: string): Array<IParsedNode> => {
+		this.importRegex.lastIndex = 0;
+
+		const
+			imports = [];
+
+		if (/(disable-sort-imports)/g.test(source)) {
+			return [];
+		}
+
+		let match;
+
+		while ((match =  this.importRegex.exec(source))) {
+			imports.push({
+				default: match[ParsedGroup.DefaultImport],
+				hasTypeKeyword: match[ParsedGroup.TypeKeyword],
+				namedImports: this.parseDestructiveImports(match[ParsedGroup.DestructingImportGroup]),
+				namespace: match[ParsedGroup.NamespaceImport],
+				path: match[ParsedGroup.FilePath],
+				range: {
+					index: match.index,
+					lastIndex: this.importRegex.lastIndex
+				}
+			});
+		}
+
+		return imports;
+	};
+
+	initRegex(): void {
+		const
+			name = `((?!\\d)(?:(?!\\s)[$\\w\\u0080-\\uFFFF]|\\\\u[\\da-fA-F]{4}|\\\\u\\{[\\da-fA-F]+\\})+)`,
+			ws = `\\s`,
+			spaceNoReturns = `[^\\S\\r\\n]`,
+			namespaceToken = `\\*\\s+as\\s+(${name})`,
+			defaultImportToken = name,
+			destructingImportToken = `(${name})(\\s+as\\s+(${name}))?`,
+			destructingImport = `{(${ws}*${destructingImportToken}(${ws}*,${ws}*${destructingImportToken})*${ws}*,?${ws}*)}`,
+			defaultAndDestructingImport = `${defaultImportToken}${ws}*,${ws}*${destructingImport}`,
+			combinedImportTypes = `(${namespaceToken}|${defaultImportToken}|${destructingImport}|${defaultAndDestructingImport})`,
+			inlineComment = `(${spaceNoReturns}*[\\/]{2}.*)?`,
+			importRegexString = `^import(\\s+type)?\\s+(${combinedImportTypes}\\s+from\\s+)?['"]([~@\\w\\\\/\.-]+)['"];?${inlineComment}\\r?\\n?`;
+
+			this.importRegex = new RegExp(importRegexString, 'gm');
+			this.destructingImportTokenRegex = new RegExp(destructingImportToken);
 	}
 
-	let match;
-
-	while ((match = importRegex.exec(source))) {
-		imports.push({
-			default: match[ParsedGroup.DefaultImport],
-			hasTypeKeyword: match[ParsedGroup.TypeKeyword],
-			namedImports: parseDestructiveImports(match[ParsedGroup.DestructingImportGroup]),
-			namespace: match[ParsedGroup.NamespaceImport],
-			path: match[ParsedGroup.FilePath],
-			range: {
-				index: match.index,
-				lastIndex: importRegex.lastIndex
-			}
-		});
-	}
-
-	return imports;
-};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {parseImportNodes} from './Parser';
+import {Parser} from './Parser';
 import fs from 'fs';
 
 const
+	parser = new Parser(),
 	fileInput = fs.readFileSync(__dirname + '/input.ts', 'utf-8'),
-	nodes = parseImportNodes(fileInput);
+	nodes = parser.parseImportNodes(fileInput);
 
 for (const node of nodes) {
 	console.log(node);
