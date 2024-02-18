506ae321233f5de5dfea80709ce2fe9a574152c7
Jakub Záruba <eflyax42@gmail.com>
2024-02-18 19:37:39 +0100

    Feature/vs code extension (#3)
    * WIP
    
    * VSCode extension packaging => vsix
    
    * Build
    
    ---------
    
    Co-authored-by: Jakub Záruba <jakuba.zaruba@orgis.cz>

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import {Parser} from './core/Parser';
-
-const
-	parser = new Parser(),
-	output = parser.getOutputForSourceFile(__dirname + '/../tests/inputScenarios/scenario3.ts');
-
-console.log(output);
