fb4d4b00eb4fc218f3910033911ef20ef577b5c4
Jakub Záruba <eflyax42@gmail.com>
2024-02-18 19:34:46 +0100

    VSCode extension packaging => vsix

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,25 @@
+import {Parser} from './core/Parser';
 import * as vscode from 'vscode';
-
-let
-	sortOnSaveDisposer: vscode.Disposable;
 
 export const activate = (context: vscode.ExtensionContext) => {
 	const command = vscode.commands.registerCommand('extension.sortYmports', () => {
 		const
+			parser = new Parser(),
 			editor = vscode.window.activeTextEditor,
 			document = editor.document,
-			rawText = document.getText();
+			rawText = document.getText(),
+			result = parser.getOutputForSource(rawText);
 
-			console.log(rawText)
+		vscode.window.activeTextEditor.edit(builder => {
+			builder.replace(
+				new vscode.Range(
+					document.lineAt(0).range.start,
+					document.lineAt(document.lineCount - 1).range.end
+				),
+				result
+			);
+		});
 	});
-
-	console.log({command});
 
 	context.subscriptions.push(command);
 };
